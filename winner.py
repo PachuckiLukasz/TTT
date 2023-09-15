d2fc8ad3a47c448b7f3f950ea98e42dd9c802d20
LukaszPachucki <pachucki.lukasz97@gmail.com>
2023-09-15 11:43:30 +0200

    bug with displaying draw messages while player won with last move

--- winner.py
+++ winner.py
@@ -8,40 +8,60 @@ def checkWinner():
     y_pos = 0
     for z in lib.markers:
         # rows
-        if sum(z) == 3:
-            lib.winner = 1
-            lib.game_over = True
-            player_won = True
-        if sum(z) == -3:
-            lib.winner = 2
-            lib.game_over = True
-            player_won = True
-            # columns
-        if lib.markers[0][y_pos] + lib.markers[1][y_pos] + lib.markers[2][y_pos] == 3:
-            lib.winner = 1
-            lib.game_over = True
-            player_won = True
-        if lib.markers[0][y_pos] + lib.markers[1][y_pos] + lib.markers[2][y_pos] == -3:
-            lib.winner = 2
-            lib.game_over = True
-            player_won = True
+        player_won = horizontal_check(z, player_won)  # Check for horizontal win
+        # columns
+        player_won = vertical_check(y_pos, player_won)  # Check for vertical win
         y_pos += 1
         # cross
-        if lib.markers[0][0] + lib.markers[1][1] + lib.markers[2][2] == 3 or lib.markers[2][0] + lib.markers[1][1] + \
-                lib.markers[0][2] == 3:
-            lib.winner = 1
-            lib.game_over = True
-            player_won = True
-        if lib.markers[0][0] + lib.markers[1][1] + lib.markers[2][2] == -3 or lib.markers[2][0] + lib.markers[1][1] + \
-                lib.markers[0][2] == -3:
-            lib.winner = 2
-            lib.game_over = True
-            player_won = True
+        player_won = cross_check(player_won)  # Check for cross win
 
+    # Check for draw only if no player has won
+    if not player_won:
+        check_for_draw(player_won)
+
+
+def cross_check(player_won):
+    if lib.markers[0][0] + lib.markers[1][1] + lib.markers[2][2] == 3 or lib.markers[2][0] + lib.markers[1][1] + \
+            lib.markers[0][2] == 3:
+        lib.winner = 1
+        lib.game_over = True
+        player_won = True
+    if lib.markers[0][0] + lib.markers[1][1] + lib.markers[2][2] == -3 or lib.markers[2][0] + lib.markers[1][1] + \
+            lib.markers[0][2] == -3:
+        lib.winner = 2
+        lib.game_over = True
+        player_won = True
+    return player_won
+
+
+def vertical_check(y_pos, player_won):
+    if lib.markers[0][y_pos] + lib.markers[1][y_pos] + lib.markers[2][y_pos] == 3:
+        lib.winner = 1
+        lib.game_over = True
+        player_won = True
+    if lib.markers[0][y_pos] + lib.markers[1][y_pos] + lib.markers[2][y_pos] == -3:
+        lib.winner = 2
+        lib.game_over = True
+        player_won = True
+    return player_won
+
+
+def horizontal_check(z, player_won):
+    if sum(z) == 3:
+        lib.winner = 1
+        lib.game_over = True
+        player_won = True
+    if sum(z) == -3:
+        lib.winner = 2
+        lib.game_over = True
+        player_won = True
+    return player_won
+
+
+def check_for_draw(player_won):
     if all(cell != 0 for row in lib.markers for cell in row) and not player_won:
         lib.winner = 0
         lib.game_over = True
-        return
 
 
 def ai_random_move():
